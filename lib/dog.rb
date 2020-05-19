bd3bcae20e18950b3aa4f2277227510d42b0fdbc
Dallas Blackwell <mdvayne923@gmail.com>
2020-05-19 23:14:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,6 @@
 class Dog
   
+<<<<<<< HEAD
   def name=(dog_name)
     @name = dog_name
   end
@@ -14,5 +15,28 @@ class Dog
   
   def breed
     @breed
+=======
+  def initialize(name)
+    @name = name
+  end
+  
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+      puts "Fido"
+  end
+  
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+      puts "Snoopy"
+  end
+  
+  def breed=(dog_breed)
+    @this_dogs_bark = dog_breed
+  end  
+  
+  def breed
+    @this_dogs_bark
+      puts "Beagle"
+>>>>>>> 0008616527b204f5de200d07906ef88fafc1ca75
   end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Person
   def name=(person_name)
     @name = person_name
@@ -15,3 +16,63 @@ class Person
     @job
   end
 end
+=======
+# class Person
+ 
+#   def initialize(name)
+#     @name = name
+#   end
+ 
+#   def name
+#     @name
+#   end
+ 
+#   def name=(new_name)
+#     @name = new_name
+#   end
+# end
+      # ~~~~~~~~~~~~~~~~~~ #
+class Person
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def name=(name)
+    @this_name = name
+      puts "Beyonce"
+  end
+
+  def name
+    @this_name
+  end
+  
+  def job=(person_job)
+    @this_name = person_job
+  end  
+  
+  def job
+    @this_name
+      puts "_"
+  end
+end
+
+# class Person
+ 
+#   def initialize(first_name, last_name)
+#     @first_name = first_name
+#     @last_name = last_name
+#   end
+ 
+#   def name=(full_name)
+#     first_name, last_name = full_name.split
+#     @first_name = first_name
+#     @last_name = last_name
+#   end
+ 
+#   def name
+#     "#{@first_name} #{@last_name}".strip
+#   end
+ 
+# end
+>>>>>>> 0008616527b204f5de200d07906ef88fafc1ca75
